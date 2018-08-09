cf72d9b4459499e923a1d47e7d01834f9cca2ecc
League <League@AdminisorsiMac2.attlocal.net>
2018-08-09 14:54:49 -0700

    goofy name

--- src/algorithms/GoofyNames.java
+++ src/algorithms/GoofyNames.java
@@ -11,29 +11,32 @@ public class GoofyNames {
 		String goofyName = "";
 
 		// 1. Ask the user to enter their name
-String name=JOptionPane.showInputDialog("Enter your name");
+		String name = JOptionPane.showInputDialog("Enter your name");
 		// 2. Print upper case name to the console using .toUpperCase()
-		//    Run your program to see that this works.
-System.out.println(name.toUpperCase());
+		// Run your program to see that this works.
+		name = name.toUpperCase();
 		// 3. Loop through each character of the name (steps 4 - 6).
-for (int i = 0; i < .length; i++) {
-	
-}
-//    HINT: Use .length() to determine the number of characters in the String.
-
-			// 4. Create a String variable to store the next character of the name
-			//    using .substring(start, end)
-			//    HINT: replace 'start' and 'end' to get String with character at i 
-			
-			// 5. Use MODULO to set this variable to upper case for EVEN characters
-			//    and lower case for ODD characters.
-
-			
-			// 6. ADD this String  (containing 1 char) to the goofyName String
-
+		for (int i = 0; i < name.length() - 1; i += 2) {
+			String a = name.substring(i, i + 1);
+			goofyName += a.toLowerCase();
+			String s = name.substring(i + 1, i + 2);
+			goofyName += s;
+		}
+		System.out.println(goofyName);
 		
+		
+		// HINT: Use .length() to determine the number of characters in the String.
+
+		// 4. Create a String variable to store the next character of the name
+		// using .substring(start, end)
+		// HINT: replace 'start' and 'end' to get String with character at i
+
+		// 5. Use MODULO to set this variable to upper case for EVEN characters
+		// and lower case for ODD characters.
+
+		// 6. ADD this String (containing 1 char) to the goofyName String
+
 		// 7. Use pop-up to show user their Goofy name
 
 	}
 }
-

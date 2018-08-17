8f6a3e9f1076c2deef391e8d991cb07c20f278cb
Rohan890 <league@WorthyMini-5.attlocal.net>
2018-08-17 14:57:47 -0700

    forloop/skillpractice

--- src/ForloopGauntlet.java
+++ src/ForloopGauntlet.java
@@ -1,11 +1,11 @@
 
 public class ForloopGauntlet {
-public static void main(String[] args) {
-	for(int i = 0; i<3; i++) {
-		for(int j = 0; j<3; j++) {
-	System.out.println(i+""+j);		
+	public static void main(String[] args) {
+		for (int i = 0; i < 6; i += 1) {
+			for (int j = 0; j < i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
 		}
 	}
-	}
 }
-
